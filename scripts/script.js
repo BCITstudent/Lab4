aa5ceca629ef1bad3afc607faf0cae232115b35a
Andrew Yoon <yoonandrew416@gmail.com>
2022-11-11 10:14:59 -0800

    New object to array

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
-const courseList = [{"code": "COMM 1116-0", "name": "Business Communications",}, {"code": "ACIT 1630", "name": "Database Systems",}, {"code": "ACIT 1620", "name": "Web Fundamental Technologies",}]
+const course = [{code: "COMM 1116-0", name: "Business Communications",}, {code: "ACIT 1630", "name": "Database Systems",}, {code: "ACIT 1620", name: "Web Fundamental Technologies",}]
 var num1 = prompt("Enter a 4-digit number: ");
-let
-if (hour < 18) {
-    num1 = "Yes I am taking the course: " + num1;
+
+while ((courseList != code) && (courseList != name)) {
+    courseList = prompt(course + " is not valid. Please enter a number: ");
+}
+
+if (courseList === num1) {
+    num1 = "Yes I am taking the course: " + code + name;
   } else {
-    num1 = "Good evening";
+    for (let course of course)
+    {
+        if (course.program === "CIT" && course.GPA > 3)
+        {
+            counter++;
+            console.log(course);
+        }
+    }
   }
-var keys = Object.keys(o);   
-var values = []            
-for(var i = 0; i < keys.length; i++) { 
-    var key = keys[i];                
-    values[i] = o[key];                
-}
-while ((courseList != "code") && (courseList != "paper") && (userChoice != "scissors")) {
-    userChoice = prompt(userChoice + " is not valid. Please pick: rock or paper or scissors");
-}
+console.log(counter)
