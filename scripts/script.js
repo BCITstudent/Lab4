23b75102c9be1ce3124043eab293bcca23fab122
Andrew Yoon <yoonandrew416@gmail.com>
2022-11-11 07:25:47 -0800

    Defined a constant array for course code and name

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const courseList = [{"code": "COMM 1116-0", "name": "Business Communications",}, {"code": "ACIT 1630", "name": "Database Systems",}, {"code": "ACIT 1620", "name": "Web Fundamental Technologies",}]
+var num1 = prompt("Enter a 4-digit number: ");
+let
+if (hour < 18) {
+    num1 = "Yes I am taking the course: " + num1;
+  } else {
+    num1 = "Good evening";
+  }
